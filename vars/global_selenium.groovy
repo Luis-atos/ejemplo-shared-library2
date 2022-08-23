f4fa9ee34bcff11a9859682a8aabca90c2d25e98
Luis-atos <104129252+Luis-atos@users.noreply.github.com>
2022-08-23 13:32:17 +0200

    Update global_selenium.groovy

--- vars/global_selenium.groovy
+++ vars/global_selenium.groovy
@@ -4,7 +4,7 @@ import org.cl.*
 
 def call(String param1, String param2){
     println '******************************'
-    println 'Ejecución de selenium pipeline'
+    println 'Ejecucion de selenium pipeline'
     println '******************************'
     pipeline{
         agent any
@@ -14,36 +14,29 @@ def call(String param1, String param2){
                     script{
                         try {
 
-                           println 'Inicio'
-
-                            stage('Inicio'){
+                          
                                  println '******************************'
-                                 println 'Ejecución de selenium pipeline'
+                                 println 'Ejecucion de selenium pipeline'
                                  println '******************************'
                                 println 'Inicio'
                                 println 'String 1: ' + param1
                                 println 'String 2: ' + param2
-
-                               
-                            }
-
-                            stage('Union'){
-                                println 'Union de 2 Strings: '         
-                            }
-
-                            stage('MostrarNombre'){
-                                println 'Nombre obtenido desde Json: '
-                            }
-                            stage('MostrarNombre222'){
-                                println 'Nombre obtenido222 desde222 Json: '
-                            }
-
 
                         } catch(Exception e) {
                             error ('Ha ocurrido el siguiente error: ' + e)
                         }
                     }
                 }
+            }
+            stage('Union'){
+                                println 'Union de 2 Strings: '         
+            }
+
+            stage('MostrarNombre'){
+                                println 'Nombre obtenido desde Json: '
+            }
+            stage('MostrarNombre222'){
+                                println 'Nombre obtenido222 desde222 Json: '
             }
         }
     }
