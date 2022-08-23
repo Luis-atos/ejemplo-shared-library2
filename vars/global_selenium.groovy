ca83969240f3d100b619e4542c10c3dce59faa8c
Luis-atos <104129252+Luis-atos@users.noreply.github.com>
2022-08-23 13:13:49 +0200

    Create global_selenium.groovy

---
+++ vars/global_selenium.groovy
@@ -0,0 +1,49 @@
+//global.groovy
+
+import org.cl.*
+
+def call(String param1, String param2){
+
+    println 'Ejecución de Pipeline'
+
+    pipeline{
+        agent any
+        stages{
+            stage('Pipeline'){
+                steps{
+                    script{
+                        try {
+
+                            def funciones   = new Funciones()
+
+                            stage('Inicio'){
+                                println 'Inicio'
+                                println 'String 1: ' + param1
+                                println 'String 2: ' + param2
+
+                                gradle.call()
+                            }
+
+                            stage('Union'){
+                                println 'Union de 2 Strings: ' + funciones.unirDosStrings(param1, param2)          
+                            }
+
+                            stage('MostrarNombre'){
+                                println 'Nombre obtenido desde Json: ' + funciones.mostrarNombre()
+                            }
+                            stage('MostrarNombre222'){
+                                println 'Nombre obtenido222 desde222 Json: ' + funciones.mostrarNombre()
+                            }
+
+
+                        } catch(Exception e) {
+                            error ('Ha ocurrido el siguiente error: ' + e)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+return this;
