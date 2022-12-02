33d3a0228abb45f9ed2f30246f42d8ea42c0f30d
Luis-atos <104129252+Luis-atos@users.noreply.github.com>
2022-12-02 14:24:00 +0100

    Create maven_nuevo_params.groovy

---
+++ vars/maven_nuevo_params.groovy
@@ -0,0 +1,54 @@
+pipeline {
+     agent any
+	 tools{
+	   maven "apache-maven-3.8.6"
+	   jdk "jdk1.8.0_321"
+	 }
+    stages{
+        stage("inicializa"){
+		steps{
+		  echo " **** variables ****** de MAVEN ****** "
+		}
+		}
+	    stage("tres"){
+        steps{
+                    script{
+			
+			def pathWS = pwd()
+			echo " **** ${pathWS} ****** GIT JSON LIBRERIAS ****** "
+			checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: "master"]],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [[
+                            $class: 'RelativeTargetDirectory',
+				relativeTargetDir: "/var/lib/jenkins/workspace/pipeline_prueba"
+                        ]],
+                        submoduleCfg: [],
+                        userRemoteConfigs: [[
+                            credentialsId: 'jenkins120_GitLab',
+                            url: 'git@git.servdev.mdef.es:sistemas/experimentos/papelera/newmachine120.git'
+                        ]]
+                    ])
+			
+		    }
+		}
+    }
+        stage("One"){
+            steps {
+            script{
+			
+             sleep 2
+             echo 'hello'
+			 sh 'mvn -Djdk.tls.maxCertificateChainLength=20 -Djavax.net.ssl.trustStore=/etc/pki/ca-trust/extracted/java/cacerts -Djava.net.ssl.trustStorePassword=changeit -f pom.xml clean compile'
+          }
+            }
+        }
+    stage("dos"){
+          steps {
+         echo 'dos dos'
+          }
+    }
+    
+    }
+}
