340bff891a31524f396220852a8cf37360d7c9c4
Luis-atos <104129252+Luis-atos@users.noreply.github.com>
2022-12-15 15:17:03 +0100

    Update maven_nuevo_params.groovy

--- vars/maven_nuevo_params.groovy
+++ vars/maven_nuevo_params.groovy
@@ -18,6 +18,64 @@ pipeline {
 			echo " **** ${pathWS} ****** GIT JSON LIBRERIAS ****** "
 			checkout([
                         $class: 'GitSCM',
+                        branches: [[name: "feature_selenium"]],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [[
+                            $class: 'RelativeTargetDirectory',
+				relativeTargetDir: "/var/lib/jenkins/workspace/pipeline_prueba"
+                        ]],
+                        submoduleCfg: [],
+                        userRemoteConfigs: [[
+                            credentialsId: 'jenkins120_GitLab',
+                            url: 'git@git.servdev.mdef.es:sistemas/experimentos/papelera/pruebatest.git'
+                        ]]
+                    ])
+			
+		    }
+		}
+    }
+        stage("One"){
+            steps {
+            script{
+			
+             sleep 2
+             echo 'hello'
+			 sh 'mvn -Djdk.tls.maxCertificateChainLength=20 -Djavax.net.ssl.trustStore=/etc/pki/ca-trust/extracted/java/cacerts -Djava.net.ssl.trustStorePassword=changeit -f pom.xml clean install dependency:copy-dependencies sonar:sonar -Dsonar.login=Developer -Dsonar.password=Developer'
+			 sleep 5
+			 sh "java -cp 'target/dependency/testng-7.4.0.jar:target/dependency/jcommander-1.81.jar:target/dependency/jquery-3.5.1.jar:${env.WORKSPACE}/target/classes:target/surefire-reports/*' org.testng.TestNG ${env.WORKSPACE}/testng.xml"
+          }
+            }
+        }
+    stage("dos"){
+          steps {
+         echo 'dos dos'
+          }
+    }
+    
+    }
+}
+
+***********
+pipeline {
+     agent any
+	 tools{
+	   maven "apache-maven-3.8.6"
+	   jdk "jdk1.8.0_321"
+	 }
+    stages{
+        stage("inicializa"){
+		steps{
+		  echo " **** variables ****** de MAVEN ****** "
+		}
+		}
+	    stage("tres"){
+        steps{
+                    script{
+			
+			def pathWS = pwd()
+			echo " **** ${pathWS} ****** GIT JSON LIBRERIAS ****** "
+			checkout([
+                        $class: 'GitSCM',
                         branches: [[name: "master"]],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [[
