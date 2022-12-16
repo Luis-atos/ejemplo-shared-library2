6a3b3aa42d4746c35b18cd1c19ca81185fc3ba2b
Luis-atos <104129252+Luis-atos@users.noreply.github.com>
2022-12-16 11:10:02 +0100

    Update maven_nuevo_params.groovy

--- vars/maven_nuevo_params.groovy
+++ vars/maven_nuevo_params.groovy
@@ -42,7 +42,10 @@ pipeline {
              sleep 2
              echo 'hello'
 		         def output = sh(script: 'mvn -Djdk.tls.maxCertificateChainLength=20 -Djavax.net.ssl.trustStore=/etc/pki/ca-trust/extracted/java/cacerts -Djava.net.ssl.trustStorePassword=changeit -f pom.xml clean install dependency:copy-dependencies sonar:sonar -Dsonar.login=Developer -Dsonar.password=Developer', returnStdout: true)
+		          echo "---->" + taskUrl
 		         taskUrl = output.find(~"http://divindesonar.mdef.es:9000/api/ce/task\\?id=[\\w-]*")
+		         echo "---->" + taskUrl
+		         def sonarData = readJSON text: sh(script: "curl -u e137cb4e88e85bc1a25684db220b9e1110ac8ac0: " + taskUrl, returnStdout: true)
 			// sh 'mvn -Djdk.tls.maxCertificateChainLength=20 -Djavax.net.ssl.trustStore=/etc/pki/ca-trust/extracted/java/cacerts -Djava.net.ssl.trustStorePassword=changeit -f pom.xml clean install dependency:copy-dependencies sonar:sonar -Dsonar.login=Developer -Dsonar.password=Developer'
 			 sleep 5
 			 sh "java -cp 'target/dependency/testng-7.4.0.jar:target/dependency/jcommander-1.81.jar:target/dependency/jquery-3.5.1.jar:${env.WORKSPACE}/target/classes:target/surefire-reports/*' org.testng.TestNG ${env.WORKSPACE}/testng.xml"
@@ -52,16 +55,12 @@ pipeline {
     stage("dos"){
           steps {
          echo 'dos dos'
-		//  def output = sh(script: "mvn -f source/${RutaPom}pom.xml clean install ${ParametrosMaven} sonar:sonar -Dsonar.login=Developer -Dsonar.password=Developer ${paramSonar}", returnStdout: true)
-		//  url = output.find(~"http://divindesonar.mdef.es:9000/api/ce/task\\?id=[\\w-]*")
-		  def sonarData = readJSON text: sh(script: "curl -u Sonarqube_analysis: " + taskUrl, returnStdout: true)
-		  echo "---->" + sonarData.toString()
+
           }
     }
     
     }
 }
-
 ***********
 pipeline {
      agent any
